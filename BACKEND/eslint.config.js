f2b448b9181992acd35417758921adbc22a6ba6d
Keegan-Scorgie <scorgiekeegan@gmail.com>
2024-11-10 20:33:20 +0200

    changed directory

---
+++ BACKEND/eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    env: {
+      node: true,
+      es2021: true,
+    },
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      // Add custom rules here if needed
+    },
+  };
+  
